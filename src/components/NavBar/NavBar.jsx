7cd4129e678e684893ad2a8faaa4ffee6410377a
Chinwe Watkins <chinwewatkins@chinwes-mbp.attlocal.net>
2020-07-13 14:28:42 -0500

    update

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = ({ user, handleLogout }) => {
     let nav = user ?
     <>
         <nav>
-            <div className="nav-wrapper">
+            <div className="blue nav-wrapper">
                 <ul id="nav-mobile" className="right">
                     <li><a href=" " className="nav-link">Welcome, {user.name}</a></li>
                     <li><a href=" " className="nav-link" onClick={handleLogout}>Log Out</a></li>
@@ -15,7 +15,7 @@ const NavBar = ({ user, handleLogout }) => {
     :
     <>
         <nav>
-            <div className="nav-wrapper">
+            <div className="blue nav-wrapper">
                 <ul id="nav-mobile" className="right">
                     <li><a href="/login" className="nav-link">Log In</a></li>
                     <li><a href="/signup" className="nav-link">Sign Up</a></li>
